d0995a745a475b4400470d6151c6c88368ac341c
Madeleine Schönemann <madde@MADDE-2.local>
2020-06-09 21:50:53 +0200

    Make accessibility worse

--- public/script.js
+++ public/script.js
@@ -5,18 +5,15 @@ window.onload = function () {
     const config = {
         classTo: 'input',
         errorClass: 'error', 
-        errorTextParent: 'input', 
-        
+        successClass: 'success'        
     }
 
-    // create the pristine instance
     var pristine = new Pristine(form, config);
 
     form.addEventListener('submit', function (e) {
        e.preventDefault();
        
-       // check if the form is valid
-       var valid = pristine.validate(); // returns true or false
+       var valid = pristine.validate();
        console.log(valid)
 
     });
