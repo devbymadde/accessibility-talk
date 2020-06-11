874a5d9ddc24a542946e292bc4b83315e0084995
Madeleine <madeleine.schonemann@gmail.com>
2020-06-11 13:36:57 +0200

    Clear form after submitting

--- public/script.js
+++ public/script.js
@@ -1,20 +1,30 @@
 window.onload = function () {
+  var form = document.getElementById("form");
 
-    var form = document.getElementById("form");
+  const config = {
+    classTo: "input",
+    errorClass: "error",
+    successClass: "success",
+    errorTextParent: "input",
+  };
 
-    const config = {
-        classTo: 'input',
-        errorClass: 'error', 
-        successClass: 'success'        
+  var pristine = new Pristine(form, config);
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("chameleon").checked = false;
+
+    var valid = pristine.validate();
+    if (valid) {
+      alert("Successfully submitted form!");
+
+      document.getElementById("firstName").value = "";
+      document.getElementById("lastName").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("password").value = "";
+      document.getElementById("chameleon").checked = false;
+      document.getElementById("pug").checked = false;
+      document.getElementById("penguin").checked = false;
     }
-
-    var pristine = new Pristine(form, config);
-
-    form.addEventListener('submit', function (e) {
-       e.preventDefault();
-       
-       var valid = pristine.validate();
-       console.log(valid)
-
-    });
+  });
 };
